52837c1a4b1516ab6ce1440107f4b3b041963827
Illia Tsybin <tsybin@shernet.sheridancollege.ca>
2019-07-20 14:44:23 -0400

    Add project files.

---
+++ Assign3/WebForm1.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+namespace Assign3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlCommand cmd = new SqlCommand();
+        SqlConnection con = new SqlConnection();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
